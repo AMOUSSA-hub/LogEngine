4e3f394fc48771e8adc3a998d09325e24d4bc941
amoussa <amoussa.rayan77130@hotmail.fr>
2025-07-01 19:19:35 +0200

    développement de l'API  sans la Bdd:  requête POST /logs faite.

--- API/app/main.py
+++ API/app/main.py
@@ -1,11 +1,31 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from pydantic import BaseModel
+from enum import Enum
 
 
+logs= []
 
 app= FastAPI()
 
+class LogLevel(str, Enum):
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    DEBUG = "DEBUG"
+
+class Log(BaseModel):
+    id : int = None
+    message: str
+    timestamp: str = None # = datetime.now().isoformat()
+    level: LogLevel
+    service: str
+
 @app.get("/")
-async def root():
-    return {"message": "Bienvenue sur cette API !"}
+def root():
+    return {"message": f"Bienvenue sur cette API ! Date du jour: {datetime.now().strftime('%Y.%m.%d')}"}
 
-
+@app.post("/logs")
+def create_log(log: Log):
+    logs.append(log)
+    return logs
